22e9406efe5ad64937d4fdf7a8f18c9d9fa47e11
hanklee979 <hanklee979@gmail.com>
2014-08-04 23:38:05 +0800

    完成(取消繪圖功能)

--- src/hw2_applet.java
+++ src/hw2_applet.java
@@ -9,7 +9,7 @@ import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.awt.Font;
 
-// 220min pm05:47
+// 260min pm05:47
 
 public class hw2_applet extends JApplet {
 	public hw2_applet() {
@@ -18,7 +18,6 @@ public class hw2_applet extends JApplet {
 	private JTextField textfield_number_start;
 	private JTextField textfield_number_end;
 	private JTextArea input_matrix = new JTextArea();
-	private JTextArea textArea_1 = new JTextArea();
 	private JTextArea textArea_2 = new JTextArea();
 	private JTextArea textArea_3 = new JTextArea();
 
@@ -127,68 +126,115 @@ public class hw2_applet extends JApplet {
 		label_1.setBounds(10, 35, 440, 15);
 		getContentPane().add(label_1);
 		JLabel label_3 = new JLabel("\u8F38\u5165\u8D77\u9EDE\u7DE8\u865F\uFF1A");
-		label_3.setBounds(16, 283, 97, 15);
+		label_3.setBounds(16, 167, 97, 15);
 		getContentPane().add(label_3);
 		JLabel label_4 = new JLabel(" \uFF0C\u7D42\u9EDE\uFF1A");
-		label_4.setBounds(216, 283, 66, 15);
+		label_4.setBounds(216, 167, 66, 15);
 		getContentPane().add(label_4);
 		
 		// textfield
 		textfield_number_start = new JTextField();
-		textfield_number_start.setBounds(110, 280, 96, 21);
+		textfield_number_start.setBounds(110, 164, 96, 21);
 		getContentPane().add(textfield_number_start);
 		textfield_number_start.setColumns(10);
 		
 		textfield_number_end = new JTextField();
-		textfield_number_end.setBounds(274, 280, 96, 21);
+		textfield_number_end.setBounds(274, 164, 96, 21);
 		getContentPane().add(textfield_number_end);
 		textfield_number_end.setColumns(10);
-		input_matrix.setTabSize(6);
-		input_matrix.setFont(new Font("Monospaced", Font.PLAIN, 12));
+		
 		
 		// textarea
 		input_matrix.setBounds(55, 52, 327, 102);
 		getContentPane().add(input_matrix);
-		textArea_1.setFont(new Font("Monospaced", Font.PLAIN, 12));
-		textArea_1.setTabSize(6);
+		textArea_2.setFont(new Font("Monospaced", Font.PLAIN, 11));
 		
-		textArea_1.setBounds(58, 188, 327, 84);
-		getContentPane().add(textArea_1);
-		textArea_2.setTabSize(6);
-		textArea_2.setFont(new Font("Monospaced", Font.PLAIN, 12));
-		
-		textArea_2.setBounds(58, 328, 327, 87);
+		textArea_2.setBounds(58, 212, 327, 98);
 		getContentPane().add(textArea_2);
 		textArea_3.setTabSize(4);
 		textArea_3.setFont(new Font("Monospaced", Font.PLAIN, 11));
 		
-		textArea_3.setBounds(55, 446, 327, 291);
+		textArea_3.setBounds(55, 353, 327, 325);
 		getContentPane().add(textArea_3);
-		
-		
-		// button
-		
-		// show picture
-		JButton paint = new JButton("\u7E6A\u88FD\u6709\u5411\u5716");
-		paint.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		paint.setBounds(10, 158, 105, 23);
-		getContentPane().add(paint);
 		
 		
 		// show route
 		JButton show_route = new JButton("\u627E\u51FA\u6240\u6709\u8DEF\u5F91");
 		show_route.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				deal_matrix();
+				
 				// get start and end node
 				 number_start = Integer.valueOf( textfield_number_start.getText() );
 				 number_end   = Integer.valueOf( textfield_number_end.getText() );
 				
+				// record line 
+				int n1 = 0,n2 = 0,n3 = 0; 
+				 
+				textArea_2.setText("節點 " + number_start + " 至節點 " + number_end + " 之所有路徑：\n");
+				
+				if(number_start == number_end)
+					textArea_2.setText( textArea_2.getText() + "(1) 經1個邊: 無符合路線\n" );					
+				else if(number_start > Math.sqrt(Array_AfterSplit.length) || number_end > Math.sqrt(Array_AfterSplit.length))	
+					textArea_2.setText( textArea_2.getText() + "(1) 經1個邊: 無符合路線\n" );	
+				else
+					textArea_2.setText( textArea_2.getText() + "(1) 經1個邊:" + number_start + "->" + number_end + "\n");
+				
+				// 判定路線是否存在，若是則記錄經過的點
+				for(int i = 4 ; i >= 0 ; i--){
+					if(A[number_start - 1][i] > 0 && A[i][number_end - 1] > 0 && i != (number_start-1) && i != (number_end-1)){
+						n1 = i+1;
+					}
+				}
+				
+				if(n1 != 0)
+					textArea_2.setText( textArea_2.getText() + "(2) 經2個邊:" + number_start + "->" + n1 + "->" + number_end+ "\n");					
+				else
+					textArea_2.setText( textArea_2.getText() + "(2) 經2個邊: 無符合路線\n") ;
+				
+				// initialize
+				n1 = 0;
+				
+				// 判定路線是否存在，若是則記錄經過的點
+				for(int i = 4; i >= 0 ;i--){
+					for(int k = 4; k >= 0 ;k--){
+						if(A[number_start-1][i] > 0 && A[i][k] > 0 && A[k][number_end-1] > 0 && i != (number_start-1) && i != (number_end-1) && k != (number_start-1) && k != (number_end-1)){
+							n1 = i+1;
+							n2 = k+1;
+						}
+					}
+				}
+				
+				if(n1 != 0 && n2 != 0)
+					textArea_2.setText( textArea_2.getText() + "(3) 經3個邊:" + number_start + "->" + n1 + "->" + n2 + "->" + number_end+ "\n");					
+				else
+					textArea_2.setText( textArea_2.getText() + "(3) 經3個邊: 無符合路線\n") ;
+				
+				// initialize
+				n1 = 0;
+				n2 = 0;
+
+				// 判定路線是否存在，若是則記錄經過的點
+				for(int i = 4; i >= 0 ;i--){
+					for(int j = 4; j >= 0 ;j--){
+						for(int k = 4; k >= 0 ;k--){		
+							if(A[number_start-1][i] > 0 && A[i][j] > 0 && A[j][k] > 0 && A[k][number_end-1] > 0 && i != (number_start-1) && i != (number_end-1) && j != (number_start-1) && j != (number_end-1)&& k != (number_start-1) && k != (number_end-1)){
+								n1 = i+1;
+								n2 = j+1;
+								n3 = k+1;
+							}
+						}
+					}
+				}		
+				
+				if(n1 != 0 && n2 != 0 && n3 != 0)
+					textArea_2.setText( textArea_2.getText() + "(4) 經4個邊:" + number_start + "->" + n1 + "->" + n2 + "->" + n3 + "->" + number_end+ "\n");					
+				else
+					textArea_2.setText( textArea_2.getText() + "(4) 經4個邊: 無符合路線\n") ;
+				
 			}
 		});
-		show_route.setBounds(13, 303, 118, 23);
+		show_route.setBounds(13, 187, 118, 23);
 		getContentPane().add(show_route);
 		
 		
@@ -238,7 +284,7 @@ public class hw2_applet extends JApplet {
 				
 			}				
 		});
-		show_result.setBounds(10, 421, 148, 23);
+		show_result.setBounds(10, 320, 148, 23);
 		getContentPane().add(show_result);
 		
 		
@@ -246,15 +292,24 @@ public class hw2_applet extends JApplet {
 		JButton clear = new JButton("\u6E05\u9664\u6240\u6709\u5C0D\u8A71\u6846\u5167\u5BB9 ");
 		clear.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				input_matrix.setText ( "" ) ;
-				textArea_1.setText ( "" ) ;			
+				input_matrix.setText ( "" ) ;		
 				textArea_2.setText ( "" ) ;		
 				textArea_3.setText ( "" ) ;						
 				textfield_number_start.setText ( "" );
 				textfield_number_end.setText ( "" );
+				
+				for(int i = 0 ; i < 5 ; i++){
+					for(int j = 0 ; j < 5 ; j++){
+						A[i][j] = 0;
+						A2[i][j] = 0;
+						A3[i][j] = 0;
+						A4[i][j] = 0;
+						sum[i][j]= 0;
+					}
+				}
 			}
 		});
-		clear.setBounds(55, 738, 158, 22);
+		clear.setBounds(55, 688, 158, 22);
 		getContentPane().add(clear);
 		
 		
@@ -268,7 +323,7 @@ public class hw2_applet extends JApplet {
 									 + "0,1,0,0"  );			
 			}
 		});
-		create_default.setBounds(260, 738, 120, 22);
+		create_default.setBounds(260, 688, 120, 22);
 		getContentPane().add(create_default);
 
 
